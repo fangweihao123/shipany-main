3be21c4b44982cf99f7e0b95a2968c446e312aa5
fangweihao123 <mmrpy4vy6ey5u139876s4ys4zbdusa@bots.bitbucket.org>
2025-09-17 10:11:13 +0800

    删除api-sdk里面的设置，直接在service里面去做好了

--- src/aisdk/provider/image-model/image-model-provider.ts
+++
@@ -1,8 +0,0 @@
-export const PROVIDER_CONFIGS  = {
-  nanobanana: {
-    name: "nano banana",
-    description: "Advanced Image Generation Engine",
-    maxFileSize: 10 * 1024 * 1024,
-    endPoint: '/api/service/gen-image'
-  }
-};
