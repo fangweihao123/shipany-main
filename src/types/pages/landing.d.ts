f8c3d939c0f1087d7f12b4e972d2dfae7310c824
fangweihao123 <fwh19970312@gmail.com>
2025-10-10 14:04:14 +0800

    [fea-102-sora]添加主页组件为图片和视频生成组件

--- src/types/pages/landing.d.ts
+++ src/types/pages/landing.d.ts
@@ -4,11 +4,15 @@ import { Section } from "@/types/blocks/section";
 import { Footer } from "@/types/blocks/footer";
 import { Pricing } from "@/types/blocks/pricing";
 import { Detection } from "@/types/blocks/detect"
+import { HeroGeometic } from "@/types/blocks/heroGeometic";
+import { ImageGenerator } from "@/types/blocks/imagegenerator";
 
 export interface LandingPage {
   header?: Header;
   hero?: Hero;
+  heroGeometic?: HeroGeometic;
   detection?: Detection;
+  imageGenerator?: ImageGenerator;
   branding?: Section;
   introduce?: Section;
   benefit?: Section;
