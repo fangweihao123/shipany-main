86b6002fd8ca0fe4ca96a019840fde2047ee61b7
fangweihao123 <fwh19970312@gmail.com>
2025-10-14 22:21:13 +0800

    [fea-sora-115]添加使用业务

--- src/services/constant.ts
+++ src/services/constant.ts
@@ -17,3 +17,21 @@ export const AffiliateRewardAmount = {
   Invited: 0,
   Paied: 5000, // $50
 };
+
+export const TrialTaskMaxAttempts = {
+  GenerateVideo: 1
+} as const;
+
+export const TaskCreditsConsumption = {
+  nanobananat2i: 2,
+  nanobananai2i: 2,
+  nanobananai2v: 2,
+  sora2i2v: 10,
+  sora2t2v: 10
+} as const;
+
+export const ErrorCode = {
+  InSufficientCredits: 100,
+  APIError: 200,
+  RunOutTrial: 300
+}
