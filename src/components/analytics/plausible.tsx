c90a902ce834883f18ccbc8904d1fbd0e3b48bc6
fangweihao123 <fwh19970312@gmail.com>
2025-10-16 10:31:19 +0800

    Revert "[fea-banana-103]更新plausible组件"
    This reverts commit 03b5f5c7700125ed3c5ca6ecdda58ad802b70063.

--- src/components/analytics/plausible.tsx
+++ src/components/analytics/plausible.tsx
@@ -1,18 +1,20 @@
-'use client';
-
-import { useEffect } from 'react';
-import { init } from '@plausible-analytics/tracker'
-
 export default function Plausible() {
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      const domain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
-      const scriptUrl = process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_URL;
-      if (domain && scriptUrl) {
-        init({ domain });
-      }
-    }
-  }, []);
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
 
-  return null;
+  const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+  const plausibleScriptUrl = process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_URL;
+
+  if (!plausibleDomain || !plausibleScriptUrl) {
+    return null;
+  }
+
+  return (
+    <script
+      defer
+      data-domain={plausibleDomain}
+      src={plausibleScriptUrl}
+    ></script>
+  );
 }
